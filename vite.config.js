5d668febdc69ef513fb708cbdad053c55bcdf3a2
benmulligan4NU <c1006420@newcastle.ac.uk>
2026-01-05 13:10:59 +0100

    update vite config for custom domain

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
-  base: mode === "production" ? "/benmulligandev/" : "/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
